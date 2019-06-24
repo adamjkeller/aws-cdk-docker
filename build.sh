1b84697246a696085965a06e3be87cb5485dd4bd
Keller <keladam@f01898543b21.ant.amazon.com>
2019-06-24 12:35:38 -0700

    v.35.0

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Add version here, and it will be built
-cdk_versions=('0.33.0' '0.34.0')
+cdk_versions=('0.33.0' '0.34.0' '0.35.0')
 
 for version in "${cdk_versions[@]}";do
     echo "docker build --build-arg CDK_VERSION=$version -t adam9098/aws-cdk:"v$version""
