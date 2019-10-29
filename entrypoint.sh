5a24b69e0a0df30553541f78a2a860420f63b872
Adam Keller <keladam@amazon.com>
2019-10-29 09:54:01 -0700

    Adding env_var.sh to entrypoint.sh as an option to source env variables in the container. Also added ability to install custom python modules on runtime (not great, but ok for now) by looking for requirements-add.txt to entrypoint script.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,13 @@ if [[ $1 == "bash" ]];then
     echo "entering container..."
     /bin/bash
 else
+    # Sourcing external env vars
+    if [[ -f env_vars.sh ]]; then
+        source env_vars.sh
+    fi
+    # Adding any additional requirements
+    if [[ -f requirements-add.txt ]]; then
+        pip3 install -r requirements-add.txt
+    fi
     exec cdk $@
 fi
